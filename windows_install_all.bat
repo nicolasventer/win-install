686116609b1a439440b31cdd56d1bd44f32f25e7
Nicolas VENTER <thedeal78@hotmail.fr>
2022-02-27 12:16:07 +0100

    v1

---
+++ windows_install_all.bat
@@ -0,0 +1,50 @@
+@REM utility
+winget install -e --id Mozilla.Firefox
+winget install -e --id 7zip.7zip
+winget install -e --id VideoLAN.VLC
+winget install -e --id Discord.Discord
+winget install -e --id ONLYOFFICE.DesktopEditors
+winget install -e --id Adobe.Acrobat.Reader.64-bit
+winget install -e --id SublimeHQ.SublimeText.4
+winget install -e --id WinDirStat.WinDirStat
+winget install -e --id Audacity.Audacity
+
+@REM utility+
+winget install -e --id AnyDeskSoftwareGmbH.AnyDesk --force
+winget install -e --id OBSProject.OBSStudio
+winget install -e --id SublimeHQ.SublimeMerge
+winget install -e --id HakuNeko.HakuNeko
+start "" https://ffmpeg.org/download.html
+winget install -e --id ImageMagick.ImageMagick
+winget install -e --id Stremio.Stremio
+start "" https://jdownloader.org/download/index
+winget install -e --id NickeManarin.ScreenToGif
+winget install -e --id den4b.ReNamer
+
+@REM game
+winget install -e --id Valve.Steam
+start "" https://ds4-windows.com/download/ryochan7-ds4windows/
+winget install -e --id EpicGames.EpicGamesLauncher
+winget install -e --id RiotGames.LeagueOfLegends.EUW
+
+@REM game+
+start "" https://www.overwolf.com/
+
+@REM programming
+winget install -e --id Git.Git
+winget install -e --id Microsoft.VisualStudio.2022.Community-Preview
+start "" https://code.visualstudio.com/Download
+winget install -e --id Python.Python.3
+winget install -e --id OpenJS.NodeJS
+start "" https://winlibs.com/
+winget install -e --id LLVM.LLVM
+
+@REM programming+
+winget install -e --id Meld.Meld
+winget install -e --id Graphviz.Graphviz
+
+
+@REM winget install -e --id MKVToolNix.MKVToolNix
+
+@REM https://winget.run/
+@REM winget search ""
